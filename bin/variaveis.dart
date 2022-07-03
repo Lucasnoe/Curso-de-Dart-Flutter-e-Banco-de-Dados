36cf4d689e09ecbf40d2f455ae70d2f336b85f0d
Lucasnoe <lucas.nooe@outlook.com>
2022-07-03 20:02:30 -0300

    final

--- bin/variaveis.dart
+++ bin/variaveis.dart
@@ -1,16 +1,22 @@
-import 'package:variaveis/variaveis.dart' as variaveis;
 import 'dart:io';
 
 void main(List<String> arguments) {
   print('Informe o seu nome');
-  
-  String le_nome = stdin.readLineSync() // Método std.readLineSync:  lê uma lihha do terminal e converte pra String
-  
+  String nome = stdin
+      .readLineSync()!; // Método std.readLineSync:  lê uma lihha do terminal e converte pra String
+
+  print('Informe a sua idade');
   String leidade = stdin.readLineSync()!;
-  int idade =int.parse(leidade); // Método int.parse: converte uma variável em int
-  double salario = 1800.55;
+  int idade =
+      int.parse(leidade); // Método int.parse: converte uma variável em int
+
+  print('Agora, informe seu salário');
+  String lesalario = stdin.readLineSync()!;
+  double salario = double.parse(
+      lesalario); // Método double.parse: converte uma variável em double
+
   print(
-      'Hello world: $le_nome e minha idade é: $idade, bem como meu salario é: $salario');
+      'Hello world: $nome e minha idade é: $idade, bem como meu salario é: $salario');
 }
 // O uso da exclamação ao final do readLineSync se dá, pois no Dart 3 há um recurso 
 //adicionado chamado Null Safety que é usado para garantir que a entrada não seja Null
